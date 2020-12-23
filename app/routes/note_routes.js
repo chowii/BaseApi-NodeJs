adbbda22aa845eff7bdf535be667f449f9f9491b
chowii <sabbib007madness@gmail.com>
2020-12-23 11:34:14 +1100

    implement display certain collections from certain database

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -15,9 +15,9 @@ module.exports = function(app, client) {
 		return await client.db().admin().listDatabases(cb);
 	};
 
-	app.get('/notes/:name', (req, res) => {
-		const id = req.params.name;
-		client.db(id).listCollections().toArray((err, collections) => {
+	app.get('/:name', (req, res) => {
+		const name = req.params.name;
+		client.db(name).listCollections().toArray((err, collections) => {
 			if (err) {
 				res.send({ 'error': 'An error has occured' });
 			} else {
@@ -27,14 +27,26 @@ module.exports = function(app, client) {
 		})
 	});
 
-	app.get('/notes/', (req, res) => {
-		client.db().collection('users').find({}).toArray(function(err, result) {
+	app.get('/:name/:table', (req, res) => {
+		const headers = req.headers
+		const limit = parseInt(headers.pagesize)
+		const page = parseInt(headers.page)
+		const params = req.params
+		const name = params.name;
+		const table = params.table;
+		client.db(name).collection(table).find(null, {
+			limit: limit, 
+			skip: (page * (limit - 1)),
+			projection: { title: 5, year: 1 } 
+		}).toArray(function(err, result) {
 			if (err) {
-				res.send({ 'error' : 'An error has occured' });
+				res.send({ 'error' : 'An error has occured' + err });
 			} else {
-				console.log("RESPONSE RESULT>>")
-				console.log(result);
-				res.send(result);
+				res.send({
+					'page': page,
+					'count': limit,
+					'data':result
+				});
 			}
 		})
 	});
